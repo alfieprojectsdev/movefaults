9c812ee6866f525cd49c3d3c59d5a6da489010d2
Alfie <alfieprojects.dev@gmail.com>
2026-05-05 10:26:23 +0800

    feat(brn-005): RUNX_v2 --reference-station + orchestrator velocity pipeline hook
    RUNX_v2.py had three input() gates blocking headless execution:
      1. getxyz(): "Press Enter to continue"
      2. transform(): "Input reference station:" (the critical one)
      3. plotfiles(): "Press Enter" + time.sleep(3) pause
    
    Changes:
    - start() now accepts reference_station parameter; interactive=True when
      it is None, preserving the existing staff workflow unchanged
    - All input() and time.sleep() calls gated on interactive flag
    - Added argparse with --reference-station / -r; guarded by __name__ == '__main__'
      so the module remains importable
    - BerneseOrchestrator.run_velocity_pipeline(): invokes the script headlessly
      via subprocess from a given crd_dir, passes --reference-station through;
      raises RuntimeError on non-zero exit
    - outlier_input-site.py is intentionally NOT automated per ticket spec;
      that gate is replaced by the velocity-reviewer web UI

--- analysis/02 Time Series/RUNX_v2.py
+++ analysis/02 Time Series/RUNX_v2.py
@@ -1,149 +1,170 @@
 #Date created: March 2020
 
+import argparse
 import glob, os, numpy, time
 import pymap3d
 
-def start():
+def start(reference_station=None):
+    # When reference_station is provided (headless mode), skip all input() prompts.
+    interactive = reference_station is None
 
-		def getxyz():
-				print('\t ======================================================================== \n')
-				print('\t \t \t \t WELCOME GPS TEAM! :) \n')
-				print('\t Transform ECEF (XYZ) to ENU (East-North-Up) coordinates using PYMAP3D \n')
-				print('\t About this version: The local ENU coordinates are fixed to your selected \n \t \t \t \t reference station.')
-				print('\t \t INPUT: CRD files \t \t OUTPUT: PLOT files \n')
-				print('\t ======================================================================== \n')
-				input('\t Press Enter to continue \n')
-				print('\t ========================= Getting XYZ coordinates ====================== \n')
-				for files in glob.glob('*.CRD'):
-						with open(files) as lines:
-								for i in range(5):
-										next(lines)
-								for line in lines:
-										x = line.split()
-										if len(line.split()) == 7:
-												f = open('XYZ', 'a+')
-												f.write('{:.4s}  {:.5s}  {:>13}  {:>13}  {:>13}\n'.format(x[1], files[2:7], x[3], x[4], x[5]))
-												f.close()
-												print('\t ' +x[1]+' '+files[2:7])	
-										elif len(line.split()) == 6:
-												pass
-										else:
-												f = open('XYZ', 'a+')
-												f.write('----------------------------------------------------------\n')
-												f.close()
-		
-		def transform():
-				print('\n \t ============== Transforming XYZ coordinates to local ENU =============== \n')
-				
-				refsite = input('\t Input reference station: ')
-				refsite = refsite.upper()
+    def getxyz():
+        print('\t ======================================================================== \n')
+        print('\t \t \t \t WELCOME GPS TEAM! :) \n')
+        print('\t Transform ECEF (XYZ) to ENU (East-North-Up) coordinates using PYMAP3D \n')
+        print('\t About this version: The local ENU coordinates are fixed to your selected \n \t \t \t \t reference station.')
+        print('\t \t INPUT: CRD files \t \t OUTPUT: PLOT files \n')
+        print('\t ======================================================================== \n')
+        if interactive:
+            input('\t Press Enter to continue \n')
+        print('\t ========================= Getting XYZ coordinates ====================== \n')
+        for files in glob.glob('*.CRD'):
+            with open(files) as lines:
+                for i in range(5):
+                    next(lines)
+                for line in lines:
+                    x = line.split()
+                    if len(line.split()) == 7:
+                        f = open('XYZ', 'a+')
+                        f.write('{:.4s}  {:.5s}  {:>13}  {:>13}  {:>13}\n'.format(x[1], files[2:7], x[3], x[4], x[5]))
+                        f.close()
+                        print('\t ' +x[1]+' '+files[2:7])
+                    elif len(line.split()) == 6:
+                        pass
+                    else:
+                        f = open('XYZ', 'a+')
+                        f.write('----------------------------------------------------------\n')
+                        f.close()
 
-				for files in glob.glob('*.CRD'):
-						with open(files) as lines:
-								for i in range(6):
-										next(lines)
-								for line in lines:
-										a = line.split()
-										if len(line.split()) == 7 and a[1] == refsite:
-												X = float(a[3])
-												Y = float(a[4])
-												Z = float(a[5])
-												lat, lon, alt = pymap3d.ecef2geodetic(X, Y, Z, deg=True)
-												for file in glob.glob('*.CRD'):
-														with open(file) as line:
-																for i in range(6):
-																		next(line)
-																for lines in line:
-																		r = lines.split()
-																		if file[2:7] == files[2:7]:
-																				if len(lines.split()) == 7 and r[1] != refsite:
-																						X = float(r[3])
-																						Y = float(r[4])
-																						Z = float(r[5])
-																						east, north, up = pymap3d.ecef2enu(X, Y, Z, lat, lon, alt, deg=True)
-																						f = open('ENU', 'a')
-																						f.write('{:4s}  {:4s}  {:5s}  {:.4f}  {:.4f}  {:.4f}\n'.format(r[1], refsite, file[2:7], east, north, up))
-																				elif len(lines.split()) >= 6 and r[1] == refsite:
-																						pass
-																				elif len(lines.split()) == 6:
-																						pass
-																				else:
-																						f = open('ENU', 'a+')
-																						f.write('----------------------------------------------------------\n')
-																						f.close()
-						
-		def getenu():
-				print('\n \t ==================== Getting local ENU coordinates ===================== \n')
-				
-				sname = []
-				
-				for lines in open('ENU'):
-						x = lines.split()
-						if len(x[0]) == 4:
-								if len(sname) == 0:
-										sname.append(x[0])
-								elif len(sname) == 1:
-										del sname[0]
-										sname.append(x[0])
-								open(sname[0], 'a').write(x[2]+'  '+x[3]+'  '+x[4]+'  '+x[5])
-								open(sname[0], 'a').write('\n')
-						
-		def plotfiles():
-				input('\t To create PLOT files, press Enter')
-				
-				print('\t Running... \n')
-				
-				alldata = []
-				
-				dirName = 'PLOTS'
-				if not os.path.exists(dirName):
-						os.mkdir(dirName)
-						print('\t Directory', dirName, 'created')
-				else:
-						print('\t Directory', dirName, 'already exists')
-						pass
+    def transform():
+        print('\n \t ============== Transforming XYZ coordinates to local ENU =============== \n')
 
-				print('\n \t ======================= Creating PLOT files ============================')
-                
-				print('\n \t List of sites: ')
-				
-				for sites in glob.glob('????'):
-						print('\t '+ sites)
-						f = open('123', 'a+')
-						f.write(sites + '\n')
-						f.close()
-						for lines in open(sites):
-								alldata.append(sites+' '+lines)
-								
-				
-				os.rename(os.getcwd()+'//123', os.getcwd()+'//PLOTS'+'//123')
-				
-				os.chdir(os.getcwd()+'//PLOTS')
-				
-				for files in alldata:
-						allyear = str(files.split()[1])
-						if 00 <= int(allyear[0:2]) <=80:
-								year='20'+allyear[0:2]
-						else:
-								year='19'+allyear[0:2]
-						day = int(allyear[2:5])/365.25
-						date = int(year)+day
-						sitenames = files.split()[0]
-						coorde = str(files.split()[2])
-						coordn = str(files.split()[3])
-						coordu = str(files.split()[4])
-						if os.path.isfile(os.getcwd()+'//'+sitenames) is True:
-								sitefile = open(sitenames, 'a')
-								sitefile.write('{:.4f}  {:>13}  {:>13}  {:>13}\n'.format(date, coorde, coordn, coordu))
-						if os.path.isfile(os.getcwd()+'//'+sitenames) is False:
-								sitefile = open(sitenames, 'w')
-								sitefile.write('{:.4f}  {:>13}  {:>13}  {:>13}\n'.format(date, coorde, coordn, coordu))
-				
-				print('\n \t DONE! ')
-				time.sleep(3)
+        if reference_station is not None:
+            refsite = reference_station.upper()
+        else:
+            refsite = input('\t Input reference station: ')
+            refsite = refsite.upper()
 
-		getxyz()
-		transform()
-		getenu()
-		plotfiles()
-start()
+        for files in glob.glob('*.CRD'):
+            with open(files) as lines:
+                for i in range(6):
+                    next(lines)
+                for line in lines:
+                    a = line.split()
+                    if len(line.split()) == 7 and a[1] == refsite:
+                        X = float(a[3])
+                        Y = float(a[4])
+                        Z = float(a[5])
+                        lat, lon, alt = pymap3d.ecef2geodetic(X, Y, Z, deg=True)
+                        for file in glob.glob('*.CRD'):
+                            with open(file) as line:
+                                for i in range(6):
+                                    next(line)
+                                for lines in line:
+                                    r = lines.split()
+                                    if file[2:7] == files[2:7]:
+                                        if len(lines.split()) == 7 and r[1] != refsite:
+                                            X = float(r[3])
+                                            Y = float(r[4])
+                                            Z = float(r[5])
+                                            east, north, up = pymap3d.ecef2enu(X, Y, Z, lat, lon, alt, deg=True)
+                                            f = open('ENU', 'a')
+                                            f.write('{:4s}  {:4s}  {:5s}  {:.4f}  {:.4f}  {:.4f}\n'.format(r[1], refsite, file[2:7], east, north, up))
+                                        elif len(lines.split()) >= 6 and r[1] == refsite:
+                                            pass
+                                        elif len(lines.split()) == 6:
+                                            pass
+                                        else:
+                                            f = open('ENU', 'a+')
+                                            f.write('----------------------------------------------------------\n')
+                                            f.close()
+
+    def getenu():
+        print('\n \t ==================== Getting local ENU coordinates ===================== \n')
+
+        sname = []
+
+        for lines in open('ENU'):
+            x = lines.split()
+            if len(x[0]) == 4:
+                if len(sname) == 0:
+                    sname.append(x[0])
+                elif len(sname) == 1:
+                    del sname[0]
+                    sname.append(x[0])
+                open(sname[0], 'a').write(x[2]+'  '+x[3]+'  '+x[4]+'  '+x[5])
+                open(sname[0], 'a').write('\n')
+
+    def plotfiles():
+        if interactive:
+            input('\t To create PLOT files, press Enter')
+
+        print('\t Running... \n')
+
+        alldata = []
+
+        dirName = 'PLOTS'
+        if not os.path.exists(dirName):
+            os.mkdir(dirName)
+            print('\t Directory', dirName, 'created')
+        else:
+            print('\t Directory', dirName, 'already exists')
+            pass
+
+        print('\n \t ======================= Creating PLOT files ============================')
+
+        print('\n \t List of sites: ')
+
+        for sites in glob.glob('????'):
+            print('\t '+ sites)
+            f = open('123', 'a+')
+            f.write(sites + '\n')
+            f.close()
+            for lines in open(sites):
+                alldata.append(sites+' '+lines)
+
+
+        os.rename(os.getcwd()+'//123', os.getcwd()+'//PLOTS'+'//123')
+
+        os.chdir(os.getcwd()+'//PLOTS')
+
+        for files in alldata:
+            allyear = str(files.split()[1])
+            if 00 <= int(allyear[0:2]) <=80:
+                year='20'+allyear[0:2]
+            else:
+                year='19'+allyear[0:2]
+            day = int(allyear[2:5])/365.25
+            date = int(year)+day
+            sitenames = files.split()[0]
+            coorde = str(files.split()[2])
+            coordn = str(files.split()[3])
+            coordu = str(files.split()[4])
+            if os.path.isfile(os.getcwd()+'//'+sitenames) is True:
+                sitefile = open(sitenames, 'a')
+                sitefile.write('{:.4f}  {:>13}  {:>13}  {:>13}\n'.format(date, coorde, coordn, coordu))
+            if os.path.isfile(os.getcwd()+'//'+sitenames) is False:
+                sitefile = open(sitenames, 'w')
+                sitefile.write('{:.4f}  {:>13}  {:>13}  {:>13}\n'.format(date, coorde, coordn, coordu))
+
+        print('\n \t DONE! ')
+        if interactive:
+            time.sleep(3)
+
+    getxyz()
+    transform()
+    getenu()
+    plotfiles()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='RUNX v2 — Transform Bernese CRD files to ENU and create PLOT files'
+    )
+    parser.add_argument(
+        '--reference-station', '-r',
+        metavar='CODE',
+        help='4-char station code to use as ENU reference origin (omit for interactive prompt)',
+    )
+    args = parser.parse_args()
+    start(reference_station=args.reference_station)
